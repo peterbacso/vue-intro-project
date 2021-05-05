5e2e22f862c7aedec7c98b1ade56bfa70b754568
AzureAD\Peter-TamasBacso <peter.bacso@principal33.com>
2021-05-05 19:25:33 +0300

    updated image title.

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <img class='title-gif' title="Ziua Pământului 2021" src="https://www.google.com/logos/doodles/2021/geta-bratescus-95th-birthday-6753651837108375.2-l.png" />
+    <img class='title-gif' title="95 de ani de la nașterea Getei Brătescu" src="https://www.google.com/logos/doodles/2021/geta-bratescus-95th-birthday-6753651837108375.2-l.png" />
     <div class='input-box' v-bind:class="{ active: isActive }">
         <svg class='search-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>
         <input type="text" class="search-input" @click="myFilter()">
