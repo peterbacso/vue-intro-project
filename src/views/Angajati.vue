3161c5814376601acefa70488abe7cff32b47a20
AzureAD\Peter-TamasBacso <peter.bacso@principal33.com>
2021-05-06 12:01:53 +0300

    get angajati from api and populate table.

--- src/views/Angajati.vue
+++ src/views/Angajati.vue
@@ -1,13 +1,66 @@
 <template>
   <div class="angajati">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <table>
+      <thead>
+        <tr>
+          <td>id</td>
+          <td>firstname</td>
+          <td>lastname</td>
+          <td>email</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="angajat in angajati" :key="angajat.id">
+          <td>{{angajat.id}}</td>
+          <td>{{angajat.firstName}}</td>
+          <td>{{angajat.lastName}}</td>
+          <td>{{angajat.email}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import axios from "axios";
 
 export default {
   name: "Angajati",
+  data() {
+    return {
+      angajati: [],
+    }
+  },
+  created() {
+    this.getEmployees()
+  }, 
+  methods: {
+    getEmployees() {
+      axios.get(`https://localhost:5001/employee/Employee`)
+      .then(response => {
+        this.angajati = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+    }
+  }
 };
 </script>
+
+<style scoped lang="scss">
+table {
+  width: 100%;
+  max-width: 900px;
+  margin: 0 auto;
+  &, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+  }
+  th, td {
+    padding: 15px;
+    text-align: left;
+  }
+}
+</style>
