82ab79fa15c3bbf4947df4f48a3517075badb11a
AzureAD\Peter-TamasBacso <peter.bacso@principal33.com>
2021-05-07 11:58:09 +0300

    semicolon at the end of the line.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Toast from "vue-toastification";
-import "vue-toastification/dist/index.css"
+import "vue-toastification/dist/index.css";
 
 createApp(App).use(store).use(router).use(Toast).mount("#app");
